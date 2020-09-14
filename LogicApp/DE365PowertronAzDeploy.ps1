b1db28561757dfa63888803956ed4efd1945bd11
Roman Ganopolskyi <rga.roman@gmail.com>
2020-09-14 02:49:29 +0200

    Reduced number of parameters

--- LogicApp/DE365PowertronAzDeploy.ps1
+++ LogicApp/DE365PowertronAzDeploy.ps1
@@ -1,5 +1,6 @@
 $AzureResourceGroupName = "PDE-RGA-FO-Powertron"
-$AzureDeployTemplateFile = "basic\de365powertronrscdeploy.json"
+$AzureDeployTemplateFile = "basic\azuredeploy.json"
+$AzureDeployTemplateParameterFile = "basic\azuredeploy.parametres.json"
 #$storagePrefix = "dixf"
 #$OneDriveFolder = "/Projects/00 - In progress/978 - Staples/PIM_RTL/Preise/PRICE_INPUT"
 
@@ -13,6 +14,7 @@ New-AzResourceGroupDeployment `
   -Name de365powertron_dixfpackagecreate `
   -ResourceGroupName $AzureResourceGroupName `
   -TemplateFile $AzureDeployTemplateFile `
+  -TemplateParameterFile $parameterFile
   
 #New-AzResourceGroupDeployment `
 #  -Name de365powertron_dixfpackagecreate_serviceAPI `
